0f1f4b8cf62edc0e4a3bdea668439940fe3f7f09
garfieldua <andrey.davydenko@gmail.com>
2014-06-03 22:49:41 +0300

    Fixing imports

--- ATM/src/com/vabank/atm/ATMView.java
+++ ATM/src/com/vabank/atm/ATMView.java
@@ -1,8 +1,6 @@
 package com.vabank.atm;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
