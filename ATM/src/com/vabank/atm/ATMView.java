ada0e8c88d99352990911c8de0259a6d36404038
garfieldua <andrey.davydenko@gmail.com>
2014-06-03 18:34:51 +0300

    Added buttons

--- ATM/src/com/vabank/atm/ATMView.java
+++ ATM/src/com/vabank/atm/ATMView.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class ATMView extends JFrame {
@@ -39,8 +40,11 @@ public class ATMView extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(10, 11, 121, 23);
+		contentPane.add(btnNewButton);
 	}
-
 }
